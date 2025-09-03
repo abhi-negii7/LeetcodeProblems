14135775e34c25422f2df1d65369aff3979c12ee
Abhishek Negi <155905995+abhi-negii7@users.noreply.github.com>
2025-09-03 20:52:10 +0530

    Time: 1 ms (91.57%), Space: 44.5 MB (39.76%) - LeetHub

--- 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.java
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.java
@@ -5,16 +5,11 @@ class Solution {
         int m = grid[0].length;
 
         for (int i = 0; i < n; i++) {
-
             for (int j = 0; j < m; j++) {
-                if(i+1<n){
-                    if(grid[i][j]!=grid[i+1][j])
-                        return false;
-                }
-                 if(j+1<m){
-                    if(grid[i][j]==grid[i][j+1])
-                        return false;
-                }
+                if (i + 1 < n && grid[i][j] != grid[i + 1][j])
+                    return false;
+                if (j + 1 < m && grid[i][j] == grid[i][j + 1])
+                    return false;
             }
         }
         return true;
