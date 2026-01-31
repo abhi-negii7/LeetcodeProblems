89b613a1af435c1df60e29bdb0fc3a343fa24ce2
Abhishek Negi <155905995+abhi-negii7@users.noreply.github.com>
2026-01-31 23:04:12 +0530

    Time: 79 ms (5.14%), Space: 45.4 MB (99.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,33 +1,36 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        // k=3
+        // "ABCBCYDDZDD"
+        // output-> 7
+        int maxLen=0;
+        int i=0;
+        int j=0;
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        int maxLen = 0;
-        int conv = 0;
-        int maxFreq = 0;
-        char maxFreqCh = 'a';
+        HashMap<Character,Integer> map=new HashMap<>();
+        int n=s.length();
+        while(i<n && j<n){
+            char ch=s.charAt(j);
+            map.put(ch,map.getOrDefault(ch,0)+1);
 
-        int i = 0;
-        for (int j = 0; j < s.length(); j++) {
-
-            char currCh = s.charAt(j);
-            map.put(currCh, map.getOrDefault(currCh, 0) + 1);
-            int subArrayLen = j - i + 1;
-            if (maxFreq <= map.get(currCh)) {
-                maxFreq = map.get(currCh);
-                maxFreqCh = currCh;
-            }
-
-            conv = subArrayLen - maxFreq;
-            if (conv > k) {
-                if (s.charAt(i) == maxFreqCh) {
-                    maxFreq--;
-                }
-                map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+            // Number of replacement
+            int rep=(j-i+1)-maxFr(map);
+            while(rep>k){
+                char ch2=s.charAt(i);
+                map.put(ch2,map.get(ch2)-1);
                 i++;
+                rep=(j-i+1)-maxFr(map);
             }
-            maxLen = Math.max(maxLen, j - i + 1);
+            maxLen=Math.max(maxLen,j-i+1);
+            j++;
         }
         return maxLen;
+    }
+    public int maxFr(Map<Character,Integer> map){
+        int maxFreq=0;
+        for(Map.Entry<Character,Integer> en: map.entrySet()){
+            maxFreq=Math.max(maxFreq,en.getValue());
+        }
+        return maxFreq;
     }
 }
