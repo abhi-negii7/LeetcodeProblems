ca2b832e0477a8da05c58401852208937af28745
Abhishek Negi <155905995+abhi-negii7@users.noreply.github.com>
2026-02-03 20:21:57 +0530

    Time: 14 ms (50.38%), Space: 59.8 MB (22.92%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,31 +1,27 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
+        return smallerEqualSub(nums,k) - smallerEqualSub(nums,k-1);
+    }
 
-        int n = nums.length;
-        int[] nextOdd = new int[n];
-        nextOdd[n - 1] = n;
+    public int smallerEqualSub(int[] nums, int k) {
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] % 2 != 0)
-                nextOdd[i] = i + 1;
-            else
-                nextOdd[i] = nextOdd[i + 1];
-        }
+        int oddNo = 0;
+        int maxSub = 0;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
 
-        int countOdd = 0;
-        int totalSub = 0;
-        int l = 0;
-        for (int r = 0; r < n; r++) {
-
-            if (nums[r] % 2 != 0)
-                countOdd++;
-            while (countOdd == k) {
-                totalSub = totalSub + (nextOdd[r] - r);
-                if (nums[l] % 2 != 0)
-                    countOdd--;
-                l++;
+            if (nums[j] % 2 != 0) {
+                oddNo++;
             }
+            while (oddNo > k) {
+                if (nums[i] % 2 != 0)
+                    oddNo--;
+                i++;
+            }
+
+            maxSub += (j - i + 1);
+
         }
-        return totalSub;
+        return maxSub;
     }
 }
