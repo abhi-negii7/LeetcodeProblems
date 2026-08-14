805b41c2470756376c95c9ba8dc2c1a435d3f9e8
Abhishek Negi <155905995+abhi-negii7@users.noreply.github.com>
2026-08-14 10:44:17 +0530

    Time: 3 ms (43.02%), Space: 43.7 MB (52.56%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,19 +1,20 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
+        
+        HashMap<Character,Integer> map = new HashMap<>();
+        int i=0;
+        int maxLen = Integer.MIN_VALUE;
 
-        int maxLen = 0;
-        int freqArr[] = new int[26];
-        int i = 0;
-        for (int j = 0; j < s.length(); j++) {
-
-            char ch = s.charAt(j);
-            freqArr[ch - 'a']++;
-            while (freqArr[ch - 'a'] > 2) {
-                char startCh = s.charAt(i);
-                freqArr[startCh - 'a']--;
+        for(int j=0;j<s.length();j++){
+            char currCh = s.charAt(j);
+            map.put(currCh,map.getOrDefault(currCh,0)+1);
+            while(map.get(currCh)>2){
+                char prevCh = s.charAt(i);
+                map.put(prevCh,map.get(prevCh)-1);
+                if(map.get(prevCh)==0) map.remove(prevCh);
                 i++;
             }
-            maxLen = Math.max(maxLen, j - i + 1);
+            maxLen = Math.max(maxLen,j-i+1);
         }
         return maxLen;
     }
