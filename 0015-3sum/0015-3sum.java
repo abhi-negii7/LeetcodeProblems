93d2fe7d8e66a8062f15eac4c188c919d435d776
Abhishek Negi <155905995+abhi-negii7@users.noreply.github.com>
2026-09-12 10:55:05 +0530

    Time: 36 ms (28.3%), Space: 58.8 MB (90.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        HashSet<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
+        List<List<Integer>> ansList = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
 
-        int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum +=nums[i];
-            for(int j=i+1;j<nums.length;j++){
-                sum+=nums[j];
-                if(map.containsKey(-sum) && map.get(-sum)>i && map.get(-sum)>j){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[map.get(-sum)]);
-                    Collections.sort(list);
-                    if(!set.contains(list)){
-                        set.add(list);
-                        ans.add(list);
-                    }
-                }
-                sum-=nums[j];
+            int j = n - 1;
+            int mid = i + 1;
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            while (mid < j) {
+                int sum = nums[i] + nums[j] + nums[mid];
+                if (sum == 0) {
+                    ansList.add(Arrays.asList(nums[i], nums[mid], nums[j]));
+                    mid++;
+                    j--;
+                    while (mid < j && nums[mid] == nums[mid - 1])
+                        mid++;
+
+                    while (mid < j && nums[j] == nums[j + 1])
+                        j--;
+                } else if (sum < 0)
+                    mid++;
+                else
+                    j--;
             }
-            sum=0;
-
         }
-        return ans;
+        return ansList;
     }
 }
